dd67b1595d10cf5dd9256eebd9cb69248bf69197
MSfarmer <142194769+MSfarmer@users.noreply.github.com>
2023-09-02 12:40:45 +0800

    MyGithub add firstphoto.xml and FirstphotoMainActivity.kt

---
+++ MyGithubTest/app/src/main/java/com/example/mygithubtest/FirstphotoMainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mygithubtest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class firstphotoMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.firstphoto)
+
+        // 在这里可以进行与图像显示和用户交互相关的操作
+    }
+}

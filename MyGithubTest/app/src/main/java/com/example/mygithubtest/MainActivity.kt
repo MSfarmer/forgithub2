52984957a58fe9ef3ec4309de976bcdc30e1a37a
MSfarmer <142194769+MSfarmer@users.noreply.github.com>
2023-09-02 21:23:41 +0800

    MyGithub renamed PNG

--- MyGithubTest/app/src/main/java/com/example/mygithubtest/MainActivity.kt
+++ MyGithubTest/app/src/main/java/com/example/mygithubtest/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mygithubtest
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            val intent = Intent(this, FirstphotoMainActivity::class.java)
+            startActivity(intent)
+        }
     }
+}
+
+class FirstphotoMainActivity {
+
+}
+
+class FirstPhotoMainActivity {
+
 }
